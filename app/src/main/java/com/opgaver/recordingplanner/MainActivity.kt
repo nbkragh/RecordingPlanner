f39c1b8db9cc8377c02a5db1a2f0061497ddcb46
NBK <msungo85@gmail.com>
2021-01-02 15:42:55 +0100

    Crashlytics setup

--- app/src/main/java/com/opgaver/recordingplanner/MainActivity.kt
+++ app/src/main/java/com/opgaver/recordingplanner/MainActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     // Null until onCreateView.
     private var toPlans_Button: Button? = null
+    private var toRecordings_Button: Button? = null
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        firebaseAnalytics = Firebase.analytics
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -18,6 +25,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, PlansFrameActivity::class.java)
             startActivity(intent)
         }
+
+        toRecordings_Button = findViewById(R.id.toRecordings_button)
+        toRecordings_Button!!.setOnClickListener {
+            throw RuntimeException("Crashing this App with no survivors")
+        }
+
     }
 
 
